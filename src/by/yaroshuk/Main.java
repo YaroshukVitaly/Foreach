528cfded17972e89a0723d60504f3a98d5b2d24c
Vitalya <yaroshukvi@gmail.com>
2018-08-18 21:46:32 +0300

    Initial commit

---
+++ src/by/yaroshuk/Main.java
@@ -0,0 +1,20 @@
+package by.yaroshuk;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        a.add(1);
+        a.add(3);
+        a.add(4);
+        a.add(7);
+
+        for (int el: a) {
+            System.out.println(el);
+            
+        }
+
+    }
+}
